ddccd8ded57603b3f0363d1434f2b351b9f25246
Fabio Bento Luiz <bento_85@hotmail.com>
2019-11-07 15:49:46 +0100

    compose web and database services

---
+++ database/libfprint.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS libfprint;
+
+USE libfprint;
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `fingerprint` blob,
+  PRIMARY KEY (`id`)
+) AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+
+GRANT ALL PRIVILEGES ON libfprint.* TO 'libfprint'@'%' IDENTIFIED BY '123456';
